15059271283aefcf4e6eafdd7a19ef6a5285da3b
SalmanHossen <hossainsalman873@gmail.com>
2026-05-12 23:01:23 +0600

    (ph-node) : practice reading files with fs module

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,24 +1,26 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../services/product.service";
 
-interface Product{
-   id:number;
-   name:string;
-   description?:string;
-}
+// interface Product{
+//    id:number;
+//    name:string;
+//    description?:string;
+// }
 
 
 export const productController =(req : IncomingMessage, res  : ServerResponse)=>{
 
-      const products: Product[]=[
-         {
-            id:1,
-            name:"Product -1"
-         },
-         {
-            id:2,
-            name:"Product -2"
-         }
-      ]
+      // const products: Product[]=[
+      //    {
+      //       id:1,
+      //       name:"Product -1"
+      //    },
+      //    {
+      //       id:2,
+      //       name:"Product -2"
+      //    }
+      // ]
+      const products = readProduct()
 
       res.writeHead(200,{"content-type":"application/json"})
       res.end(JSON.stringify({
